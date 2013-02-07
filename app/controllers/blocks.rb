416554f9ecb74765ab11c4a1a2a09f43d756f112
dzucconi <damonzucconi@gmail.com>
2013-02-06 22:50:31 -0500

    Formatting

--- app/controllers/blocks.rb
+++ app/controllers/blocks.rb
@@ -1,8 +1,6 @@
 Generic.controllers :blocks do
-
   get :show, map: "/view/:id" do
     @block = Arena.block decode(params[:id])
     request.xhr? ? render("blocks/show", layout: false) : render("blocks/show")
   end
-
 end
--- app/controllers/channels.rb
+++ app/controllers/channels.rb
@@ -1,5 +1,4 @@
 Generic.controllers :channels do
-
   get :index, map: "/" do
     @channel = Arena.channel DEFAULT_CHANNEL_IDENTIFIER
     render "channels/show"
@@ -9,5 +8,4 @@ Generic.controllers :channels do
     @channel = Arena.channel decode(params[:id])
     request.xhr? ? render("channels/show", layout: false) : render("channels/show")
   end
-
 end
