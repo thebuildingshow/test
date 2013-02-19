c5c8e3e7a8103c77cb71883031f492b30a4d01ac
dzucconi <damonzucconi@gmail.com>
2013-02-18 22:13:37 -0500

    Use CachedArena

--- app/controllers/blocks.rb
+++ app/controllers/blocks.rb
@@ -1,6 +1,6 @@
 App.controllers :blocks do
   get :show, map: "/view/:id" do
-    @block = Arena.block decode(params[:id])
+    @block = CachedArena.block decode(params[:id])
     request.xhr? ? render("blocks/show", layout: false) : render("blocks/show")
   end
 end
