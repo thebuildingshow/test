68673aeff02c283bc43717356e729158d9bb345b
dzucconi <damonzucconi@gmail.com>
2013-02-18 22:13:08 -0500

    Rewrite caching wrapper

--- app/models/cached_arena.rb
+++ app/models/cached_arena.rb
@@ -1,9 +1,15 @@
-class CachedArena  
-  def self.channel(id)
-    Generic.cache.set("cached_channel_#{id}", Arena.channel(id))
-  end
+class CachedArena
+  [:channel, :block].each do |method|
+    (class << self; self; end).send(:define_method, method) do |id|
+      key = "cached_#{method}_#{id}"
+      cache = App.cache.get(key)
 
-  def self.block(id)
-    Generic.cache.set("cached_block_#{id}", Arena.block(id))
+      if cache.nil?
+        App.cache.set(key, Arena.send(method, id), expires_in: 30)
+        cache = App.cache.get(key)
+      end
+
+      return cache
+    end
   end
 end
