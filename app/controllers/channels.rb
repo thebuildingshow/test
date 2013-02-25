7d526643017bc416557e6fe6fa01470cfc135008
dzucconi <damonzucconi@gmail.com>
2013-02-24 21:28:10 -0500

    Use parens

--- app/controllers/channels.rb
+++ app/controllers/channels.rb
@@ -1,11 +1,11 @@
 App.controllers :channels do
   get :index, map: "/" do
-    @channel = CachedArena.channel DEFAULT_CHANNEL_IDENTIFIER
-    render "channels/show"
+    @channel = CachedArena.channel(DEFAULT_CHANNEL_IDENTIFIER)
+    render("channels/show")
   end
 
   get :show, map: "/:id" do
-    @channel = CachedArena.channel decode(params[:id])
+    @channel = CachedArena.channel(decode(params[:id]))
     request.xhr? ? render("channels/show", layout: false) : render("channels/show")
   end
 end
