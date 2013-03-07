f235eee6c3ada61e7f8ad648c4e048c12c49b5aa
dzucconi <damonzucconi@gmail.com>
2013-03-07 17:35:45 -0500

    Adds generic error message

--- app/app.rb
+++ app/app.rb
@@ -8,7 +8,20 @@ class App < Padrino::Application
   
   enable :sessions
 
+  disable :raise_errors
+  disable :show_exceptions
+
+  configure :development do
+    enable :raise_errors
+    enable :show_exceptions
+  end
+
   configure do 
     set :haml, ugly: :true
+  end
+
+  error 400..510 do
+    status 404
+    render("errors/generic")
   end
 end
